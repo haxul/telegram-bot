01156aeff6bc259e9e150d9c2c9abf42d9584548
haxul <s.s.starodubov@yandex.ru>
2020-02-16 14:30:13 +0400

    feat: create user entity

---
+++ src/main/java/com/haxul/telegram/entities/User.java
@@ -0,0 +1,91 @@
+package com.haxul.telegram.entities;
+
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private long chatId;
+
+    @Column
+    private int stateId;
+
+    @Column
+    private String email;
+
+    @Column
+    private String phone;
+
+    @Column
+    private boolean isAdmin;
+
+    @Column
+    private  boolean isNotified;
+
+    public User() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    public int getStateId() {
+        return stateId;
+    }
+
+    public void setStateId(int stateId) {
+        this.stateId = stateId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public boolean isAdmin() {
+        return isAdmin;
+    }
+
+    public void setAdmin(boolean admin) {
+        isAdmin = admin;
+    }
+
+    public boolean isNotified() {
+        return isNotified;
+    }
+
+    public void setNotified(boolean notified) {
+        isNotified = notified;
+    }
+}

666d691d566a0892463c2a2bf4c8e162e6df1e32
haxul <s.s.starodubov@yandex.ru>
2020-02-23 21:32:10 +0400

    feat: order

---
+++ src/main/java/com/haxul/telegram/entities/UserAddress.java
@@ -0,0 +1,52 @@
+package com.haxul.telegram.entities;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class UserAddress {
+    public UserAddress() {
+    }
+
+    public UserAddress(String city, String street, int houseNumber, int flatNumber) {
+        this.city = city;
+        this.street = street;
+        this.houseNumber = houseNumber;
+        this.flatNumber = flatNumber;
+    }
+    private String city;
+    private String street;
+    private int houseNumber;
+    private int flatNumber;
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public int getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(int houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public int getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(int flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+}
